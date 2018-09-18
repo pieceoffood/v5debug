0c6d538ac36d1fe213b026aa1af632dbedf7fc83
3038922 <31612534@qq.com>
2018-09-18 23:15:24 +0800

    PROS类库翻译完毕,明天翻译DISPLAY类库....好多啊

--- include/main.h
+++ include/main.h
@@ -1,67 +1,32 @@
 /**
- * \file main.h
- *
- * Contains common definitions and header files used throughout your PROS
- * project.
- *
- * Copyright (c) 2017-2018, Purdue University ACM SIGBots.
- * All rights reserved.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * @Author: 陈昱安
+ * @Date:   2018-09-16T00:20:58+08:00
+ * @Email:  31612534@qq.com
+ * @Last modified by:   陈昱安
+ * @Last modified time: 2018-09-18T23:14:44+08:00
  */
-
 #ifndef _PROS_MAIN_H_
 #define _PROS_MAIN_H_
 
-/**
- * If defined, some commonly used enums will have preprocessor macros which give
- * a shorter,
- * more convenient naming pattern. If this isn't desired, simply comment the
- * following line out
- *
- * For instance, E_CONTROLLER_MASTER has a shorter name: CONTROLLER_MASTER.
- * E_CONTROLLER_MASTER
- * is pedantically correct within the PROS styleguide, but not convienent for
- * most student
- * programmers.
- */
-#define PROS_USE_SIMPLE_NAMES
+#define PROS_USE_SIMPLE_NAMES //如果定义了这句话 可以使用一些更短的函数或者宏的名字
 
 /**
- * If defined, C++ literals will be available for use.
- *
- * For instance, you can do `4_m = 50` to set motor 4's target velocity to 50
+ * 如果定义，C++文字将可供使用。
+ * 例如，你可以做“4ym＝50”，将马达4的目标速度设定为50。
  */
 #define PROS_USE_LITERALS
 
 #include "api.h"
 
-/**
- * If you find doing pros::Motor() to be tedious and you'd prefer just to do
- * Motor,
- * you can use the namespace with the following commented out line.
- *
- * IMPORTANT: Only the okapi or pros namespace may be used, not both
- * concurrently!
- *            The okapi namespace will export all symbols inside the pros
- * namespace.
- */
 // using namespace pros;
 // using namespace okapi;
-
-/**
- * You should add more #includes here
- */
+//这里可以添加更多的头文件
 //#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 #ifdef __cplusplus
-/**
- * You can add C++-only headers here
- */
+//这里可以添加CPP的头文件
 //#include <iostream>
 #endif
 
-#endif  // _PROS_MAIN_H_
+#endif // _PROS_MAIN_H_
