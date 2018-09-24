a90512cd5541ec5b7f61cb6fe171c711b91aed2f
3038922 <31612534@qq.com>
2018-09-24 21:13:44 +0800

    更新了OKLIB库和内核,但是PROS的类库没改

--- include/api.h
+++ include/api.h
@@ -30,8 +30,8 @@
 
 #define PROS_VERSION_MAJOR 3
 #define PROS_VERSION_MINOR 1
-#define PROS_VERSION_PATCH 0
-#define PROS_VERSION_STRING "3.1.0"
+#define PROS_VERSION_PATCH 1
+#define PROS_VERSION_STRING "3.1.1"
 
 #define PROS_ERR (INT32_MAX)
 #define PROS_ERR_F (INFINITY)
@@ -53,4 +53,4 @@
 #include "pros/vision.hpp"
 #endif
 
-#endif // _PROS_API_H_
+#endif  // _PROS_API_H_
