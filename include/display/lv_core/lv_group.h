bea899e02ff30f5bbe4c192f875a39a52423792c
3038922 <31612534@qq.com>
2018-10-09 22:11:47 +0800

    汉化了LV_GROUH

--- include/display/lv_core/lv_group.h
+++ include/display/lv_core/lv_group.h
@@ -1,13 +1,9 @@
-/**
- * @file lv_group.h
- *
- */
-
 #ifndef LV_GROUP_H
 #define LV_GROUP_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /*********************
@@ -19,8 +15,8 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
-/*Predefined keys to control the focused object via lv_group_send(group, c)*/
-/*For compatibility in signal function define the keys regardless to LV_GROUP*/
+/*通过lv_group_send控制聚焦对象的预定义键（group，c）*/
+/*为了兼容信号功能定义键，无论LV_GROUP*/
 #define LV_GROUP_KEY_UP 17    /*0x11*/
 #define LV_GROUP_KEY_DOWN 18  /*0x12*/
 #define LV_GROUP_KEY_RIGHT 19 /*0x13*/
@@ -30,131 +26,129 @@ extern "C" {
 #define LV_GROUP_KEY_NEXT 9   /*0x09, '\t'*/
 #define LV_GROUP_KEY_PREV 11  /*0x0B, '*/
 
-#define LV_GROUP_KEY_ENTER_LONG                                                \
-  14 /*0x0E, Sent by the library if ENTER is long pressed*/
+#define LV_GROUP_KEY_ENTER_LONG 14 /*0x0E, Sent by the library if ENTER is long pressed*/
 
 #if USE_LV_GROUP != 0
-/**********************
+    /**********************
  *      TYPEDEFS
  **********************/
-struct _lv_group_t;
+    struct _lv_group_t;
 
-typedef void (*lv_group_style_mod_func_t)(lv_style_t *);
-typedef void (*lv_group_focus_cb_t)(struct _lv_group_t *);
+    typedef void (*lv_group_style_mod_func_t)(lv_style_t *);
+    typedef void (*lv_group_focus_cb_t)(struct _lv_group_t *);
 
-typedef struct _lv_group_t {
-  lv_ll_t obj_ll;       /*Linked list to store the objects in the group */
-  lv_obj_t **obj_focus; /*The object in focus*/
-  lv_group_style_mod_func_t
-      style_mod; /*A function which modifies the style of the focused object*/
-  lv_group_focus_cb_t
-      focus_cb; /*A function to call when a new object is focused (optional)*/
-  lv_style_t style_tmp; /*Stores the modified style of the focused object */
-  uint8_t frozen : 1;   /*1: can't focus to new object*/
-} lv_group_t;
+    typedef struct _lv_group_t
+    {
+        lv_ll_t obj_ll;                      /*用于存储组中对象的链接列表 */
+        lv_obj_t **obj_focus;                /*焦点对象*/
+        lv_group_style_mod_func_t style_mod; /*修改焦点对象样式的函数*/
+        lv_group_focus_cb_t focus_cb;        /*聚焦新对象时调用的函数（可选）*/
+        lv_style_t style_tmp;                /*存储焦点对象的修改样式 */
+        uint8_t frozen : 1;                  /*1: 无法关注新对象*/
+    } lv_group_t;
 
-/**********************
+    /**********************
  * GLOBAL PROTOTYPES
  **********************/
 
-/**
- * Create a new object group
- * @return pointer to the new object group
+    /**
+    *创建一个新的对象组
+    * @return指向新对象组的指针
  */
-lv_group_t *lv_group_create(void);
+    lv_group_t *lv_group_create(void);
 
-/**
- * Add an object to a group
- * @param group pointer to a group
- * @param obj pointer to an object to add
+    /**
+    *将对象添加到组
+    * @param组指针指向一个组
+    * @param obj指向要添加的对象的指针
  */
-void lv_group_add_obj(lv_group_t *group, lv_obj_t *obj);
+    void lv_group_add_obj(lv_group_t *group, lv_obj_t *obj);
 
-/**
- * Remove an object from its group
- * @param obj pointer to an object to remove
+    /**
+    *从组中删除对象
+    * @param obj指向要删除的对象的指针
  */
-void lv_group_remove_obj(lv_obj_t *obj);
+    void lv_group_remove_obj(lv_obj_t *obj);
 
-/**
- * Focus on an object (defocus the current)
- * @param obj pointer to an object to focus on
+    /**
+    *专注于物体（散焦电流）
+    * @param obj指向要关注的对象的指针
  */
-void lv_group_focus_obj(lv_obj_t *obj);
+    void lv_group_focus_obj(lv_obj_t *obj);
 
-/**
- * Focus the next object in a group (defocus the current)
- * @param group pointer to a group
+    /**
+    *将下一个对象聚焦在一个组中（使当前散焦）
+    * @param组指针指向一个组
  */
-void lv_group_focus_next(lv_group_t *group);
+    void lv_group_focus_next(lv_group_t *group);
 
-/**
- * Focus the previous object in a group (defocus the current)
- * @param group pointer to a group
+    /**
+    *将上一个对象聚焦在一个组中（使当前散焦）
+    * @param组指针指向一个组
  */
-void lv_group_focus_prev(lv_group_t *group);
+    void lv_group_focus_prev(lv_group_t *group);
 
-/**
- * Do not let to change the focus from the current object
- * @param group pointer to a group
- * @param en true: freeze, false: release freezing (normal mode)
+    /**
+    *不要让焦点从当前对象改变
+   * @param组指针指向一个组
+   * @param zh_cn true：freeze，false：释放冻结（正常模式）
  */
-void lv_group_focus_freeze(lv_group_t *group, bool en);
+    void lv_group_focus_freeze(lv_group_t *group, bool en);
 
-/**
- * Send a control character to the focuses object of a group
- * @param group pointer to a group
- * @param c a character (use LV_GROUP_KEY_.. to navigate)
+    /**
+    *将控制字符发送到组的焦点对象
+    * @param组指针指向一个组
+    * @param ca字符（使用LV_GROUP_KEY_ ..导航）
  */
-void lv_group_send_data(lv_group_t *group, uint32_t c);
+    void lv_group_send_data(lv_group_t *group, uint32_t c);
 
-/**
- * Set a function for a group which will modify the object's style if it is in
- * focus
- * @param group pointer to a group
- * @param style_mod_func the style modifier function pointer
+    /**
+    *为一个组设置一个函数，如果它在，它将修改对象的样式
+    *专注
+    * @param组指针指向一个组
+    * @param style_mod_func样式修饰符函数指针
  */
-void lv_group_set_style_mod_cb(lv_group_t *group,
-                               lv_group_style_mod_func_t style_mod_func);
+    void lv_group_set_style_mod_cb(lv_group_t *group,
+                                   lv_group_style_mod_func_t style_mod_func);
 
-/**
- * Set a function for a group which will be called when a new object is focused
- * @param group pointer to a group
- * @param focus_cb the call back function or NULL if unused
+    /**
+    *为聚焦新对象时将调用的组设置一个函数
+    * @param组指针指向一个组
+    * @param focus_cb回调函数，如果未使用则为NULL
  */
-void lv_group_set_focus_cb(lv_group_t *group, lv_group_focus_cb_t focus_cb);
+    void lv_group_set_focus_cb(lv_group_t *group, lv_group_focus_cb_t focus_cb);
 
-/**
- * Modify a style with the set 'style_mod' function. The input style remains
- * unchanged.
- * @param group pointer to group
- * @param style pointer to a style to modify
- * @return a copy of the input style but modified with the 'style_mod' function
+    /**
+    *使用设置'style_mod'功能修改样式。输入样式仍然存在
+     *不变。
+     * @param组指针指向组
+     * @param样式指针指向要修改的样式
+     * @return输入样式的副本，但使用'style_mod'函数进行修改
  */
-lv_style_t *lv_group_mod_style(lv_group_t *group, const lv_style_t *style);
+    lv_style_t *lv_group_mod_style(lv_group_t *group, const lv_style_t *style);
 
-/**
- * Get the focused object or NULL if there isn't one
- * @param group pointer to a group
- * @return pointer to the focused object
+    /**
+    *获取焦点对象或NULL（如果没有）
+    * @param组指针指向一个组
+    * @return指向焦点对象的指针
  */
-lv_obj_t *lv_group_get_focused(lv_group_t *group);
+    lv_obj_t *lv_group_get_focused(lv_group_t *group);
 
-/**
- * Get a the style modifier function of a group
- * @param group pointer to a group
- * @return pointer to the style modifier function
+    /**
+    *获取组的样式修饰符功能
+    * @param组指针指向一个组
+    * @return指向样式修饰符函数的指针
  */
-lv_group_style_mod_func_t lv_group_get_style_mod_cb(lv_group_t *group);
+    lv_group_style_mod_func_t lv_group_get_style_mod_cb(lv_group_t *group);
 
-/**
- * Get the focus callback function of a group
- * @param group pointer to a group
- * @return the call back function or NULL if not set
+    /**
+    *获取组的焦点回调功能
+    * @param组指针指向一个组
+    * @return回调函数或NULL如果没有设置et
  */
-lv_group_focus_cb_t lv_group_get_focus_cb(lv_group_t *group);
+    lv_group_focus_cb_t lv_group_get_focus_cb(lv_group_t *group);
 
-/**********************
+    /**********************
  *      MACROS
  **********************/
 
