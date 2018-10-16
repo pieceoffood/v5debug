9388f3de77074f50da57d7c6c2b398b0a1c87bf6
3038922 <31612534@qq.com>
2018-10-16 11:13:21 +0800

    清理下自动赛函数

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -3,7 +3,7 @@
  * @Date:   2018-09-16T00:20:58+08:00
  * @Email:  31612534@qq.com
  * @Last modified by:   yan
- * @Last modified time: 2018-10-16T10:32:37+08:00
+ * @Last modified time: 2018-10-16T11:11:39+08:00
  */
 
 #include "main.h"
@@ -14,8 +14,6 @@
 void autonomous()
 {
     userDisplay.createAutoPage();
-    lv_obj_t *lab = lv_label_create(userDisplay.autonomousPage, nullptr);
-    lv_label_set_text(lab, "自动赛进行中");
 }
 void UserDisplay::createAutoPage()
 {
@@ -23,5 +21,7 @@ void UserDisplay::createAutoPage()
     if (autonomousPage == nullptr)
         autonomousPage = lv_obj_create(nullptr, nullptr);
     lv_scr_load(autonomousPage);
+    lv_obj_t *lab = lv_label_create(userDisplay.autonomousPage, nullptr);
+    lv_label_set_text(lab, "自动赛进行中");
     std::cout << "create autoPage" << std::endl;
 }
