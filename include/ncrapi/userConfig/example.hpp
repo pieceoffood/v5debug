db28bbe600d4e723841f4965ffa94c2d0dd5d4f1
3038922 <31612534@qq.com>
2018-10-27 09:41:04 +0800

    修改了端口号

--- include/ncrapi/userConfig/example.hpp
+++ include/ncrapi/userConfig/example.hpp
@@ -3,7 +3,7 @@
  * @Date:   2018-10-14T12:18:39+08:00
  * @Email:  358079046@qq.com
  * @Last modified by:   yan
- * @Last modified time: 2018-10-25T15:40:22+08:00
+ * @Last modified time: 2018-10-26T19:49:27+08:00
  */
 #ifndef EXAMPLE_HPP_ //这里要改
 #define EXAMPLE_HPP_ //这里要改
@@ -13,9 +13,9 @@ const unsigned int GYRO_PORT = 1;        //陀螺仪端口号
 const unsigned int SHOOT_LIMIT_PORT = 2; //射球行程开关端口号
 //马达
 const int LF = 1;
-const int LB = 11;
+const int LB = 2;
 const int RF = 3;
-const int RB = 20;
+const int RB = 4;
 const int SHOOT_L = 5;
 const int SHOOT_R = 6;
 const int INTAKE_L = 7;
