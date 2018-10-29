aede9acd936601404ddd41c451d87d7fc954bb59
3038922 <31612534@qq.com>
2018-10-29 13:28:53 +0800

    修复漏加nullptr的构造函数

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -15,5 +15,5 @@ void autonomous()
 {
     userDisplay.delTasks();
     userDisplay.delObjs();
-    userDisplay.createUserObj(OBJ_AUTONOMOUS, true, "obj_auto", "自动赛进行中");
+    userDisplay.createUserObj(OBJ_AUTONOMOUS, true, "obj_auto", nullptr, "自动赛进行中");
 }
