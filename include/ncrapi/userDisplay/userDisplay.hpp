8639363b01271d92b17644c24046088140409e48
3038922 <31612534@qq.com>
2018-10-29 15:44:36 +0800

    把sensorinfo放枚举第一个就修复了

--- include/ncrapi/userDisplay/userDisplay.hpp
+++ include/ncrapi/userDisplay/userDisplay.hpp
@@ -14,13 +14,13 @@
 extern lv_font_t myfont;
 typedef enum obj_flag
 {
+    OBJ_SENSORSINFO, //传感器页面 为了防止误删除 需要放第一个
     OBJ_COMPETITION,
     OBJ_DISABLED,
     OBJ_AUTONOMOUS,
     OBJ_OPCONTROL,
-    OBJ_CONFIRM,     //从这里开始下面都无法lv_load_src(),因为是依附状态
-    OBJ_SENSORSINFO, //传感器页面
-    BTNM_START,      //按钮阵列
+    OBJ_CONFIRM, //从这里开始下面都无法lv_load_src(),因为是依附状态
+    BTNM_START,  //按钮阵列
     LAB_SENSORS,
     LAB_LOOP
 } obj_flag;
