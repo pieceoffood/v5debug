d9d669fdc3dbf816fc5420d12652c45104bd5181
3038922 <31612534@qq.com>
2018-11-05 21:02:45 +0800

    新增弹射和拾取模块

---
+++ include/ncrapi/generic/intake.hpp
@@ -0,0 +1,10 @@
+#ifndef INTAKE_HPP_
+#define INTAKE_HPP_
+#include "generic.hpp"
+template <size_t _nums>
+class Intake : public Generic<_nums>
+{
+  public:
+    Intake() {}
+};
+#endif /* end of include guard: INTAKE_HPP_ */
---
+++ include/ncrapi/generic/lift.hpp
@@ -0,0 +1,11 @@
+#ifndef LIFT_HPP_
+#define LIFT_HPP_
+#include "generic.hpp"
+template <size_t _nums>
+class Lift : public Generic<_nums>
+{
+  private:
+  public:
+    Lift() {}
+};
+#endif /* end of include guard: LIFT_HPP_ */
