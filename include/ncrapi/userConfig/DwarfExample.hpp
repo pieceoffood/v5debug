6a6785f233c126e9d1d8f3c28989155c5d33f21a
3038922 <31612534@qq.com>
2018-11-05 23:09:14 +0800

    估计了下参数 没行程开关总感觉不靠谱

--- include/ncrapi/userConfig/DwarfExample.hpp
+++ include/ncrapi/userConfig/DwarfExample.hpp
@@ -27,7 +27,7 @@ const int SHOOT_GEAR_VAL = 360; //驱动齿轮转多少度 编码器清零
 const int SHOOT_HOLDING = 15;
 const int SHOOT_READY_VAL = 120;
 const int SHOOT_SHOOT_VAL = 180;
-const int SHOOT_MAX_TIME = 1000; //弹射最大允许的连续驱动时间
+const int SHOOT_MAX_TIME = 500; //弹射最大允许的连续驱动时间
 const int SHOOT_WAITING_TIME = 500;
 
 #endif /* end of include guard: EXAMPLE_HPP_ */
